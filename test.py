ec611234344ae778ab00701a6aadcbd346ccd586
hailan09 <67746957+hailan09@users.noreply.github.com>
2020-11-11 16:54:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -18,3 +18,18 @@ while 1:
     s, addr = dns.accept()
     dsd = s.recv(1024)
     print(dsd)
+
+
+#通过js代码在控制台将cookie插入到浏览器
+function setCookie(str) {
+        const arr = str.split('; ');
+        arr.forEach(item => {
+            const name = item.split('=')[0];
+            const value = item.split('=')[1];
+            const exdate = new Date();
+            exdate.setDate(exdate.getDate() + 7);
+            const period = `expires=${exdate.toUTCString()}; `;
+            document.cookie = `${name}=${value}; ${period}path=/`;
+        })
+    }
+setCookie("cookie value")
